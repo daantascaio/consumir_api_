9f1edebfb0a9389a16b2ded8828269fcf1ac8474
Caio Dantas <caiodantasl.lara@gmail.com>
2023-09-20 10:45:23 -0300

    put

--- put_student.py
+++ put_student.py
@@ -5,20 +5,20 @@ from get_token import token
 _print = print
 print = pprint
 
-url = "http://127.0.0.1:3001/alunos"
+url = "http://127.0.0.1:3001/alunos/2"
 
-headers = {}  # "Authorization": token
+headers = {"Authorization": token}
 
 aluno_data = {
-    # "nome": "Goes",
-    # "sobrenome": "Vieira",
-    # "email": "goes@email.com",
-    # "idade": "50",
-    # "peso": "80.04",
-    # "altura": "1.90",
+    "nome": "Goe2",
+    "sobrenome": "Vieira",
+    "email": "goe123s@email.com",
+    "idade": "50",
+    "peso": "80.04",
+    "altura": "1.90",
 }
 
-response = requests.get(url=url, json=aluno_data, headers=headers)
+response = requests.put(url=url, json=aluno_data, headers=headers)
 
 if response.status_code >= 200 and response.status_code <= 299:
     # Sucesso
@@ -27,10 +27,7 @@ if response.status_code >= 200 and response.status_code <= 299:
     # print(response.text)
 
     response_data = response.json()
-    # print(response_data)
-    for student in response_data:
-        print(student["nome"])
-        print(student["email"])
+    print(response_data)
     # print('Bytes', response.content)
 else:
     # Erros
